29861729b563ac83604dce678e60be36df53e2b2
adi <spaced.wombat@gmail.com>
2015-08-22 21:40:21 +0200

    use js.native

--- src/main/scala/org/scalajs/package.scala
+++ src/main/scala/org/scalajs/package.scala
@@ -1,8 +1,9 @@
 package org.scalajs
 
+import scalajs.js
 //package object d3 extends d3.Base with scalajs.js.GlobalScope
 
 
 package object d3 extends scalajs.js.GlobalScope {
-  val d3: D3.Base = ???
+  val d3: D3.Base = js.native
 }
