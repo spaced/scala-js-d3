719a02cc8c09fab822337e673e53e87cc3734df1
Adi Stadelmann <spaced.wombat@gmail.com>
2015-11-13 20:41:21 +0100

    First working version of implicits

--- src/main/scala/org/singlespaced/d3js/Obs.scala
+++ src/main/scala/org/singlespaced/d3js/Obs.scala
@@ -8,17 +8,18 @@ import scala.language.implicitConversions
 
 object D3DatumObs {
 
-  @inline implicit def fromFunction1To3StringPrimitive[Datum](value: scala.Function1[Datum, String]): js.Function3[Datum, Int, Int, Primitive] =
+  @inline implicit def fromFunction1To3StringPrimitive[Datum](value: Datum => String): js.Function3[Datum, Int, Int, Primitive] =
     fromFunction1To3( value.andThen( _.asInstanceOf[Primitive]) )
 
-  @inline implicit def fromFunction1To3DoublePrimitive[Datum](value: scala.Function1[Datum, Double]): js.Function3[Datum, Int, Int, Primitive] =
+  @inline implicit def fromFunction1To3DoublePrimitive[Datum](value: Datum => Double): js.Function3[Datum, Int, Int, Primitive] =
     fromFunction1To3( value.andThen( _.asInstanceOf[Primitive]) )
 
-  @inline implicit def fromFunction1To3IntPrimitive[Datum](value: scala.Function1[Datum, Int]): js.Function3[Datum, Int, Int, Primitive] =
+  @inline implicit def fromFunction1To3IntPrimitive[Datum](value: Datum =>  Int): js.Function3[Datum, Int, Int, Primitive] =
     fromFunction1To3( value.andThen( _.asInstanceOf[Primitive]) )
 
-  @inline implicit def fromFunction1To3BooleanPrimitive[Datum](value: scala.Function1[Datum, Boolean]): js.Function3[Datum, Int, Int, Primitive] =
+  @inline implicit def fromFunction1To3BooleanPrimitive[Datum](value: Datum => Boolean): js.Function3[Datum, Int, Int, Primitive] =
     fromFunction1To3( value.andThen( _.asInstanceOf[Primitive]) )
+
 
   @inline implicit def fromFunction2To3StringPrimitive[Datum](value: scala.Function2[Datum,Int, String]): js.Function3[Datum, Int, Int, Primitive] =
     fromFunction2To3( (d:Datum,i:Int) => value.apply(d,i).asInstanceOf[Primitive] )
@@ -32,7 +33,7 @@ object D3DatumObs {
   @inline implicit def fromFunction2To3IntPrimitive[Datum](value: scala.Function2[Datum,Int, Int]): js.Function3[Datum, Int, Int, Primitive] =
     fromFunction2To3( (d:Datum,i:Int) => value.apply(d,i).asInstanceOf[Primitive] )
 
-  @inline implicit def fromFunction1To3[Datum, M](value: scala.Function1[Datum, M]): js.Function3[Datum, Int, Int, M] = {
+  @inline implicit def fromFunction1To3[Datum, M](value: Datum => M): js.Function3[Datum, Int, Int, M] = {
     val scalafun = (d: Datum, i: Int, x: Int) => value.apply(d)
     js.Any.fromFunction3[Datum, Int, Int, M](scalafun)
   }
--- src/test/scala/org/singlespaced/d3js/D3Test.scala
+++ src/test/scala/org/singlespaced/d3js/D3Test.scala
@@ -2,6 +2,7 @@ package org.singlespaced.d3js
 
 import org.scalajs.dom
 import org.singlespaced.d3js.D3DatumObs._
+import org.singlespaced.d3js.d3.Primitive
 import utest._
 
 import scala.scalajs.js
@@ -15,18 +16,13 @@ object D3Test extends TestSuite {
 
   val tests = TestSuite {
     'd3_selection_attr_fun1WithInt {
-      val f= (d:Int) => d*2
-      sel.attr("tabindex", f )
+      sel.attr("tabindex", (d:Int) => d*2 )
       assert("10" == elem.getAttribute("tabindex"))
     }
     'd3_selection_classed_fun2WithBoolean {
-      val f= (d:Int,i:Int,x:Int) => d==5
+      val f= (d:Int,i:Int) => d==5
       sel.classed("myclass", f )
       assert(elem.classList.contains("myclass"))
-    }
-    'd3_selection_attrfunx_update {
-      val f3= (d:Int) => (d*2)
-      val tt=sel.attr("tabindex", f3 )
     }
 
   }
